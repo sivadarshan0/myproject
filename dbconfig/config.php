6b45e1eceddbb0519f24c26514a0662acfc622eb
sivadarshan0 <72275180+sivadarshan0@users.noreply.github.com>
2021-01-06 23:34:25 +0530

    Update config.php

--- dbconfig/config.php
+++ dbconfig/config.php
@@ -1,4 +1,4 @@
 <?php
-$con=mysqli_connect("localhost", "root", "root") or die ("Unable to connect");
+$con=mysqli_connect("mariadb_cont", "root", "root") or die ("Unable to connect");
 mysqli_select_db($con,'mydb');
 ?>
